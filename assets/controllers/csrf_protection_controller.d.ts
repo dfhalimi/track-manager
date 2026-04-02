3aea2e3789182052b57bdd8a99c927c4a88e49b3
Deniz Halimi <halimi@joboo.de>
2026-04-02 12:39:01 +0200

    Track Anpassungen
    - Quality Checks Error Fix

---
+++ assets/controllers/csrf_protection_controller.d.ts
@@ -0,0 +1,6 @@
+export function generateCsrfToken(formElement: HTMLFormElement): void;
+export function generateCsrfHeaders(formElement: HTMLFormElement): Record<string, string>;
+export function removeCsrfToken(formElement: HTMLFormElement): void;
+
+declare const _default: "csrf-protection-controller";
+export default _default;
